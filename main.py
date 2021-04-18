acc4dd07c4f2f29c335c69d6b14bf9bcb7e0ee17
forkpoweroutlet <48189055+forkpoweroutlet@users.noreply.github.com>
2021-04-18 09:50:16 -0700

    snake_case function names + fix typo

--- main.py
+++ main.py
@@ -15,18 +15,18 @@ EXPLOSION = '💥'
 ADJACENT = ('⬛', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣')
 
 # Bounds check
-def isValid(x, y):
+def is_valid(x, y):
     return (0 <= x < GRID_WIDTH) and (0 <= y < GRID_HEIGHT)
 
 # Shorter way to write over files opened in mode r+
-def overwriteAndClose(file, text):
+def overwrite_close(file, text):
     file.seek(0)
     file.truncate()
     file.write(text)
     file.close()
 
 # Includes diagonally adjacent mines (0-8)
-def getNumAdjacentMines(x, y, mineList):
+def get_num_adjacent_mines(x, y, mineList):
     deltas = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
     numAdjacent = 0
 
@@ -38,7 +38,7 @@ def getNumAdjacentMines(x, y, mineList):
     return numAdjacent
 
 # Renders 2D list as Markdown table string
-def toMarkdown(data):
+def render_markdown(data):
     tableWidth = len(data[0])
 
     renderedField = "|" * (tableWidth + 1) + "\n|" + "-|" * tableWidth + "\n"
@@ -47,14 +47,14 @@ def toMarkdown(data):
     return renderedField
 
 # Be nice to the user on their first dig: clear out some space
-def generateMines(numMines, initialX, initialY):
+def generate_mines(numMines, initialX, initialY):
     generatedMines = []
     while len(generatedMines) < numMines:
         newMine = [randrange(0, GRID_WIDTH), randrange(0, GRID_HEIGHT)]
 
         if newMine not in generatedMines and \
             newMine != [initialX, initialY] and \
-            getNumAdjacentMines(initialX, initialY, [newMine]) == 0:
+            get_num_adjacent_mines(initialX, initialY, [newMine]) == 0:
                 generatedMines.append(newMine)
     
     return generatedMines
@@ -73,7 +73,7 @@ def main():
         point = json.loads(sys.argv[2])
         selectedX = round(point[0])
         selectedY = round(point[1])
-        if not isValid(selectedX, selectedY):
+        if not is_valid(selectedX, selectedY):
             raise ValueError("Invalid input")
 
         file = open("mines.json", "r+", encoding="utf8")
@@ -81,9 +81,9 @@ def main():
 
         # If needs new game
         if not mines:
-            mines = generateMines(40, selectedX, selectedY)
+            mines = generate_mines(40, selectedX, selectedY)
             actionsEnv = "encrypt=true"
-        overwriteAndClose(file, json.dumps(mines))
+        overwrite_close(file, json.dumps(mines))
 
         file = open("field.json", "r+", encoding="utf8")
         fieldArray = json.load(file)
@@ -129,12 +129,12 @@ def main():
                 if MYSTERY not in fieldArray[cur[1]][cur[0]] or list(cur) in mines:
                     continue
 
-                numAdjacentMines = getNumAdjacentMines(cur[0], cur[1], mines)
+                numAdjacentMines = get_num_adjacent_mines(cur[0], cur[1], mines)
                 fieldArray[cur[1]][cur[0]] = ADJACENT[numAdjacentMines]
 
                 for delta in deltas:
                     newPoint = (cur[0] + delta[0], cur[1] + delta[1])
-                    if isValid(newPoint[0], newPoint[1]) and numAdjacentMines == 0:
+                    if is_valid(newPoint[0], newPoint[1]) and numAdjacentMines == 0:
                         toSet.add(newPoint)
 
             # Check for game win, specifically that the player hasn't missed a spot
@@ -150,7 +150,7 @@ def main():
             for rowNum, rowArray in enumerate(fieldArray):
                 fieldArray[rowNum] = list(map(unlinkCell, rowArray))
               
-        overwriteAndClose(file, json.dumps(fieldArray))
+        overwrite_close(file, json.dumps(fieldArray))
 
         # Render grid to README or archive file
         filenameToWrite = "prev-game.md" if gameOver else "README.md"
@@ -158,9 +158,9 @@ def main():
         table = file.read()
         file.close()
 
-        table = table.replace("FIELD_GOES_HERE", toMarkdown(fieldArray))
+        table = table.replace("FIELD_GOES_HERE", render_markdown(fieldArray))
         file = open(filenameToWrite, "r+", encoding="utf8")
-        overwriteAndClose(file, table)
+        overwrite_close(file, table)
 
         # Reset the field to an empty grid
         if gameOver:
@@ -168,7 +168,7 @@ def main():
             copyfile("template/mines.json", "mines.json")
 
             file = open("template/field.json", "r", encoding="utf8")
-            markdown = toMarkdown(json.load(file))
+            markdown = render_markdown(json.load(file))
             file.close()
 
             file = open("template/README.md", "r", encoding="utf8")
@@ -183,7 +183,7 @@ def main():
             
         actionsEnv += "\noutput_file=" + filenameToWrite + "\ndone=ok"
 
-    # We can attribute some users to improper user input, others not
+    # Some errors can be attributed to improper user input
     except json.JSONDecodeError as jsonError:
         if jsonError.doc == sys.argv[2]:
            actionsEnv += "\ndone=bad_input"
